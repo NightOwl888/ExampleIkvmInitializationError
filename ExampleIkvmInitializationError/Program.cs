a2eb222d97cc1ec87ff69f0510f0db214e52d199
Shad Storhaug <shad@shadstorhaug.com>
2024-01-13 01:16:18 +0700

    Created project

---
+++ ExampleIkvmInitializationError/Program.cs
@@ -0,0 +1,17 @@
+﻿using opennlp.tools.sentdetect;
+using System;
+
+namespace ExampleIkvmNetFrameworkError
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using System.IO.Stream stream = typeof(Program).Assembly.GetManifestResourceStream("ExampleIkvmNetFrameworkError.en-test-sent.bin");
+            var sw = new ikvm.io.InputStreamWrapper(stream);
+            new SentenceModel(sw);
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
